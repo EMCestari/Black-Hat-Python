8e427a604d4e536b212c2b87cc27fc1ff4036010
Enrico Maria Cestari <enricomaria.cestari@gmail.com>
2022-02-23 21:12:24 +0100

    Chapter 4 added

--- Chapter 3 - Writing a Sniffer/sniffer.py
+++ Chapter 3 - Writing a Sniffer/sniffer.py
@@ -2,7 +2,7 @@ import socket
 import os
 
 # host to listen to
-HOST = '127.0.0.1'
+HOST = '192.168.56.1'
 
 
 def main():
---
+++ Chapter 4 - Owning the Network with Scapy/mail_sniffer.py
@@ -0,0 +1,10 @@
+from scapy.all import sniff
+
+def packet_callback(packet):
+    print(packet.show())
+
+    def main():
+        sniff(prn=packet_callback, count=1)
+
+    if __name__ == '__main__':
+        main()
